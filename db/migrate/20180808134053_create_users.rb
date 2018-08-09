671b305c0456d300007a21df4965bc094e8e3423
Alberto Carreras <a.carreras.c@gmail.com>
2018-08-09 14:42:54 -0400

    remove uniquess username

--- db/migrate/20180808134053_create_users.rb
+++ db/migrate/20180808134053_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :username, null: false, unique: true
+      t.string :username, null: false
       t.string :email, null: false, index: true, unique: true
       t.string :password_digest
       t.string :role, null: false, default: 'user'
