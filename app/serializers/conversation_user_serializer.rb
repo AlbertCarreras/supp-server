4c09d3b1051e5c221b2e0a22be007b3f8cb54e81
Alberto Carreras <a.carreras.c@gmail.com>
2018-08-22 12:59:12 -0400

    added attributes to conversation user serializer

--- app/serializers/conversation_user_serializer.rb
+++ app/serializers/conversation_user_serializer.rb
@@ -2,11 +2,15 @@ class ConversationUserSerializer < ActiveModel::Serializer
 
   include Rails.application.routes.url_helpers
 
-  attributes :id, :username, :profile_image_url
+  attributes :id, :username, :bio, :interests, :distance, :profile_image_url
 
   def profile_image_url
     variant = object.profile_image.variant(resize: "100x100")
     return rails_representation_url(variant, only_path: true)
+  end
+
+  def distance
+    return object.distance_to(current_user)
   end
 
 end
