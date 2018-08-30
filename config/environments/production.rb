b391a813effc7c63a4a416839326a7b61edd2301
Alberto Carreras <a.carreras.c@gmail.com>
2018-08-30 15:33:36 -0400

    fix config.action_cable.url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = "wss://supp-server.herokuapp.com/api/v1/cable"
   config.web_socket_server_url = "wss://supp-server.herokuapp.com/api/v1/cable"
 
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
